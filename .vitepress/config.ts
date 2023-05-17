41d0ee6f16d22ae60139a9299cb3421c1722cc8d
Richard Shih <shiddong0724@gmail.com>
2023-05-17 10:50:08 +0800

    fix: 注释vitepress base

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   title: "Collectin Docs",
   description:
     "免费不限量的 Collectin 服务可以帮助你更好的收集和管理书签和页签，只需点击一下便可以帮你快速保存感兴趣的网页，不管是文章、图片、视频、社交媒体、应用等都能一键保存到 Collectin 服务中。",
-  base: "/collectin-docs/",
+  // base: "/collectin-docs/",
   themeConfig: {
     logo: "/icon.png",
     siteTitle: "Collectin Docs",
